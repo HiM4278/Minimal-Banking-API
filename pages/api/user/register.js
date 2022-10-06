ecafbc4108f3001ec3bab36c3edd1b3c32b464ef
pasakorn <pasakorn141@gmail.com>
2022-10-06 15:13:03 +0700

    completed

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -9,6 +9,12 @@ export default function userRegisterRoute(req, res) {
     //check authentication
     const user = checkToken(req);
     //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
+    if (!user || !user.isAdmin) {
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to create admin user",
+      });
+    }
 
     //validate body
     if (
@@ -25,11 +31,33 @@ export default function userRegisterRoute(req, res) {
     //check if username is already in database
     const users = readUsersDB();
     //return res.status(400).json({ ok: false, message: "Username is already taken" });
-
+    const foundUser = users.find((x) => x.username === username);
+    if (foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
     //create new user and add in db
+    if (isAdmin) {
+      const newUser = {
+        username,
+        password: bcrypt.hashSync(password, 12),
+        isAdmin,
+        money: null,
+      };
+      users.push(newUser);
+    } else {
+      const newUser = {
+        username,
+        password: bcrypt.hashSync(password, 12),
+        isAdmin,
+        money: 0,
+      };
+      users.push(newUser);
+    }
 
     writeUsersDB(users);
 
     //return response
+    return res.json({ ok: true, username, isAdmin });
   }
 }
--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -1,8 +1,17 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { writeUsersDB } from "../../backendLibs/dbLib";
+
 export default function withdrawRoute(req, res) {
   if (req.method === "PUT") {
     //check authentication
     //return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
-
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to deposit",
+      });
+    }
     const amount = req.body.amount;
     //validate body
     if (typeof amount !== "number")
@@ -10,10 +19,27 @@ export default function withdrawRoute(req, res) {
 
     //check if amount < 1
     // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
-
+    if (amount < 1) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Amount must be greater than 0" });
+    }
     //find and update money in DB (if user has enough money)
-    //return res.status(400).json({ ok: false, message: "You do not has enough money" });
+    const users = readUsersDB();
+    const foundUser = users.find((x) => x.username === user.username);
 
+    if (amount > foundUser.money) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "You do not has enough money" });
+    }
+    //return res.status(400).json({ ok: false, message: "You do not has enough money" });
+    foundUser.money = foundUser.money - amount;
+    if (foundUser.money < 0) {
+      foundUser.money = 0;
+    }
+    writeUsersDB(users);
+    return res.status(200).json({ ok: true, money: foundUser.money });
     //return response
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
